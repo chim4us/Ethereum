2ff86b1bc63197e3e531c0560486bdb924760e7d
Frank Chimdi <33489950+chim4us@users.noreply.github.com>
2020-07-05 21:54:47 +0100

    Create Lottery.sol
    Original Code for Lottery tree on eth

---
+++ Lottery.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.17;
+
+contract Lottery{
+    address public manager;
+    address[] public players;
+    
+    //string[] public MyArray;
+    
+    constructor()public {
+        manager = msg.sender;
+        //MyArray.push("Hi");
+    } 
+    
+    function enter() public payable{
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+    
+    function ramdom() private view returns(uint){
+        return uint(keccak256(block.difficulty, now, players));
+    }
+    
+    function pickWinner() public rstricted{
+        uint index = ramdom() % players.length;
+        require(condition, “message to be displayed”);
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+    
+    modifier rstricted(){
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function getPlayers() public view returns (address[]){
+        return players;
+    }
+    //function getArray() public view returns (string[]){
+    //    return MyArray;
+    //}
+}
