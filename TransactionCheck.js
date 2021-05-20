948b8935881db487303e80cf67ac816e40bbd1f7
Frank Chimdi <33489950+chim4us@users.noreply.github.com>
2021-05-20 12:13:35 +0100

    Update TransactionCheck.js
    Updated to pagination

--- TransactionCheck.js
+++ TransactionCheck.js
@@ -1,32 +1,44 @@
 const Web3 = require('web3');
 
-class TransactionChecker{
+let accountsgas =  [];
+let current_page = 1;
+let row = 5;
 
-  constructor(projectId,account){
-    this.web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + projectId));
-    this.account = account.toLowerCase();
-  }
-
-  async checkBlock(){
+async function checkBlock(projectId,accounts) {
+  this.web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + projectId));
+  for (var i = 0; i < accounts.length; i++) {
+    var account = accounts[i].toLowerCase();
+    //Get the latest block
     let block = await this.web3.eth.getBlock('latest');
-    let gasUsed = block.gasUsed;
-    let number = block.number;
-    console.log('Searching block' + number);
-
-    if(block != null && block.transactions != null){
-      for(let txHash of block.transactions){
-        let tx = await this.web3.eth.getTransaction(txHash);
-        if(this.account == tx.to.toLowerCase()){
-          console.log('Gas used for this transaction is ' + gasUsed);
-          console.log({address: tx.from, value: this.web3.utils.fromwei(tx.value,'ether'), timestamp: new Date()})
+    let bnumber = block.number;
+    let fblock = block.number - 5760;
+    let tgasUsed = 0;
+    //loop through the blocks mine today
+    for(var b = fblock; b <= bnumber; b++){
+      let cblock = await this.web3.eth.getBlock(b);
+      let gasUsed = cblock.gasUsed;
+      if(cblock != null && cblock.transactions != null){
+        for(let txHash of cblock.transactions){
+          let tx = await this.web3.eth.getTransaction(txHash);
+          if(this.account == tx.from.toLowerCase()){
+            tgasUsed += gasUsed;
+          }
         }
       }
     }
+    //Push the result to an array
+    accountsgas.push({Address: account,gasused: tgasUsed})
   }
 }
 
-let txChecker = new TransactionChecker('78dffa81deeb4e25b4a1413708d6f3d1','0x976855ddE53e42326CC5F0641ef8a3dF0c505CFb');
+function DisplayList(item,rows_per_page,page){
+  page --;
+  let loop_start = rows_per_page * page;
+  let paginatedItems = item.slice(loop_start,loop_start + rows_per_page);
+  console.log(paginatedItems);
 
-setInterval(()=>{
-  txChecker.checkBlock();
-}, 15 * 1000);
+}
+
+checkBlock('78dffa81deeb4e25b4a1413708d6f3d1', ['0x976855ddE53e42326CC5F0641ef8a3dF0c505CFb','0x976855ddE53e42326CC5F0641ef8a3dF0c505CFb','0x976855ddE53e42326CC5F0641ef8a3dF0c505CFb']);
+
+DisplayList(accountsgas,row,current_page);
